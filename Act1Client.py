0fc09f8035c8fc80825aa1e6c08623438950a0e7
L200183161 <44853905+L200183161@users.noreply.github.com>
2018-12-12 19:09:06 +0700

    Prak10

---
+++ Act1Client.py
@@ -0,0 +1,20 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("",50001))
+s.listen(5)
+print "Program komunikasi tentang data diri"
+data = ''
+kamus = {'nama':'Donny Rizal',
+         'NIM':'L200183161',
+         'angkatan':'2018',
+         'keluar':'siap!!'}
+while data.lower() != 'keluar':
+    komm, addr = s.accept()
+    while data.lower() != 'keluar':
+        data = komm.recv(1024)
+        print 'Perintah: ',data
+        if kamus.has_key(data):
+            komm.send(kamus[data])
+        else:
+            komm.send('Maaf, perintah tidak dimengerti')
